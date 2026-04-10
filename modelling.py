182ded9efc1a409b983b8207fdbae31d8253bca0
rfahur11 <rfahrur6045@gmail.com>
2026-04-10 14:25:39 +0700

    tes

--- modelling.py
+++ modelling.py
@@ -22,6 +22,7 @@ if __name__ == "__main__":
     random_state=42,
     test_size=0.2
     )
+    
     input_example = X_train[0:5]
     n_estimators = int(sys.argv[1]) if len(sys.argv) > 1 else 505
     max_depth = int(sys.argv[2]) if len(sys.argv) > 2 else 37
